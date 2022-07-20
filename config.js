ed947adac4f3a53eacb1816d2dbe46c2a551aeb1
K4lash <62519513+El-K4lash@users.noreply.github.com>
2022-07-20 17:05:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     token: "OTk3NDk3MDM5MDIzNzcxNjk5.Grx5su.orAjygFJuBPH7I2WLtcEN5Ppk2Jh1kdyVk8tlE",
     prefix: ";",
     owners: ["972636327365451796", "913670584049758248","708271219954417725"],
-    authLink: "https://discord.com/api/oauth2/authorize?client_id=997497039023771699&redirect_uri=https%3A%2F%2FAuth-spy3.heicheik.repl.co&response_type=code&scope=identify%20guilds.join",
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=997497039023771699&redirect_uri=https%3A%2F%2F.repl.co&response_type=code&scope=identify%20guilds.join",
 
 }
